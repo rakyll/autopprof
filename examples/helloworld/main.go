85e8af835c91a31b585fc99f546762dff02c973c
JBD <jbd@golang.org>
2018-11-21 15:21:05 -0800

    Add more realistic example

--- examples/helloworld/main.go
+++ examples/helloworld/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"strconv"
 	"time"
 
 	"github.com/rakyll/autopprof"
 )
 
 func main() {
-	autopprof.Capture(autopprof.HeapProfile{})
+	autopprof.Capture(autopprof.CPUProfile{
+		Duration: 15 * time.Second,
+	})
 
-	time.Sleep(time.Hour)
+	for i := 0; i < 5000; i++ {
+		generateID(5, 1000)
+	}
+}
+
+func generateID(duration int, usage int) {
+	for j := 0; j < duration; j++ {
+		go func() {
+			for i := 0; i < usage*80000; i++ {
+				str := "str" + strconv.Itoa(i)
+				str = str + "a"
+			}
+		}()
+		time.Sleep(1 * time.Second)
+	}
 }
